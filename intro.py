421f0fc7204e7a619cec3f27345dac05b25f8d0c
anudeepgadari07 <anudeepgadari@gmail.com>
2026-02-20 00:22:30 +0530

    Initial commit

---
+++ intro.py
@@ -0,0 +1,4 @@
+'''OOPS - Object Oriented Programming is a Paradigm/Approach to programming.
+It is a way of organizing code where data(attributes) and functions(methods) are combained
+together in a class. It models real world entities as software objects that emphazies 
+relationship,encapsulation,inheritance and polymorphism.'''
